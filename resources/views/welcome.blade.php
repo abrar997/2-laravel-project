7cb4061e5d2a79c072037327341db9e2b1cb0fa3
Abrar Muthanna Rakea <83250161+abrar997@users.noreply.github.com>
2022-04-13 23:44:11 +0300

    2-laravelproject

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>new project</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+</head>
+<body class="antialiased">
+<h1 style="color:red;text-align: center">hello aboddy </h1>
+<p style="color:green;text-align: center">how are u what are you doing ?? </p>
+</body>
+</html>
+{{--php artisan serve ==>to open serve --}}
